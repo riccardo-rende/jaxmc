64814cb300c52f00a314e519f26dea0a0147ab5b
Riccardo Rende <rendericcardo@gmail.com>
2022-08-28 18:40:28 +0200

    added fori_loop in energy

--- jaxmc/hamiltonian.py
+++ jaxmc/hamiltonian.py
@@ -14,11 +14,12 @@ def e_local_nn_1d(i:int, spins: jnp.array):
 
 @partial(jax.jit, static_argnums=(1,))
 def energy(spins: jnp.array, e_local: Callable) -> float:
+    def body_fn(i:int, vals: tuple[jnp.ndarray, jnp.ndarray], e_local: Callable):
+        e, spins = vals
+        return [e + e_local(i, spins), spins]
+
     L = jnp.size(spins)
 
-    e = 0.0
-
-    for i in range(L):
-        e = e + e_local(i, spins)
-    e = 0.5 * e
-    return e
+    init_vals = [0.0, spins]
+    vals = jax.lax.fori_loop(0, L, partial(body_fn, e_local=e_local), init_vals)
+    return 0.5 * vals[0]
